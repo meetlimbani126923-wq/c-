b262f111724b1ded68bd09772461d3e80b00deae
meetlimbani126923-wq <meet.limbani126923@marwadiuniversity.ac.in>
2026-02-24 08:39:41 +0530

    Add files via upload

---
+++ UNIT-4 solutin/UNIT-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIT_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangle r = new rectangle();
+            r.getdata();
+            r.calculate();
+            r.display();
+
+            cricle c = new cricle();
+            c.getdata();
+            c.cal();
+            c.display();
+
+
+        }
+    }
+}
---
+++ UNIT-4 solutin/UNIT-4/cricle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNIT_4
+{
+    internal class cricle
+    {
+        int r;
+        double area;
+
+        public void getdata()
+        {
+            Console.WriteLine("enter radious");
+            r = int.Parse(Console.ReadLine());
+        }
+        public void cal()
+        {
+            area = 3.14 * r * r;
+        }
+        public void display()
+        {
+            Console.WriteLine("area = " + area);
+        }
+
+
+    }      
+}
