8bd61c4e67055d2f86d27211e6992417c7256d2d
meetlimbani126923-wq <meet.limbani126923@marwadiuniversity.ac.in>
2026-02-24 08:39:06 +0530

    Add files via upload

---
+++ pro13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro13
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           int[] a = new int[10];
+            
+            Console.WriteLine("enter 10 value");
+
+            int i;
+            for(i = 0; i<10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("revers");
+            for (i = a.Length - 1; i > 0; i--)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+    }
+}
---
+++ pro14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro14
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name;
+
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+
+            string revname = " ";
+            int i;
+            for (i = name.Length - 1; i >= 0; i--)
+            {
+                revname = revname + name[i];
+            }
+            if (name == revname)
+            {
+                Console.WriteLine("palidrom");
+            }
+            else
+            {
+                Console.WriteLine("not palidrom");
+            }
+        }
+    }
+}
