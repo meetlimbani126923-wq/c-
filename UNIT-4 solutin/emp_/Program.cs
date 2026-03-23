7d5b40de00f8f8d6b32c6ea1a64273ebe66a86ba
meetlimbani126923-wq <meet.limbani126923@marwadiuniversity.ac.in>
2026-03-23 10:03:21 +0530

    Add files via upload

---
+++ UNIT-4 solutin/emp_/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            details d = new details();
+            d.emp_details();
+            d.display();
+
+        }
+    }
+}
---
+++ UNIT-4 solutin/emp_/details.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp_
+{
+    internal class details
+    {
+        int eid;
+        int econ;
+        string ename;
+
+        public void emp_details()
+        {
+            Console.WriteLine(" enter Employee name ");
+            ename = Console.ReadLine();
+
+            Console.WriteLine(" enter Employee contact number ");
+            econ = (int)Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine(" enter employee id ");
+            eid = Convert.ToInt32(Console.ReadLine());
+        
+        }
+        public void display()
+        {
+            Console.WriteLine(" enter empioyee name" + ename );
+            Console.WriteLine(" enter employee contact number" + econ );
+            Console.WriteLine(" enter employee id" + eid );
+
+                
+        }
+    }
+}
