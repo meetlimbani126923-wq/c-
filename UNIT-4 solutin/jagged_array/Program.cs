8151ea98c244fba5df7ef5f5178928d7c5220694
meetlimbani126923-wq <meet.limbani126923@marwadiuniversity.ac.in>
2026-03-28 13:49:00 +0530

    Add files via upload

---
+++ UNIT-4 solutin/jagged_array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jagged_array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[][] j1 = new int[3][];
+
+            j1[0] = new int[3];
+            j1[1] = new int[4];
+            j1[2] = new int[1];
+
+            for (int i = 0; i < j1.Length; i++)
+            {
+                for (int j = 0; j < j1[i].Length; j++)
+                {
+                    Console.WriteLine("enter value ");
+                    j1[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("value are ......");
+
+            for (int i = 0; i < j1.Length; i++)
+            {
+                for (int j = 0; j < j1[i].Length; j++)
+                {
+                    Console.Write(j1[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ UNIT-4 solutin/u4p2/car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u4p2
+{
+    internal class car:vihical
+    {
+        string model_type, compny_name;
+
+        public void get_car()
+        {
+            Console.WriteLine("Enter car type --> ");
+            model_type = Console.ReadLine();
+
+            Console.WriteLine("Enter compny name --> ");
+            compny_name = Console.ReadLine();
+        }
+
+        public void show_car()
+        {
+            Console.WriteLine("Car type -->   " + model_type);
+            Console.WriteLine("Car compny --> " + compny_name);
+        }
+    }
+}
