62ed379d63ea2de9aca43a6e46a3234fae2d0ab6
meetlimbani126923-wq <meet.limbani126923@marwadiuniversity.ac.in>
2026-03-28 13:47:58 +0530

    Add files via upload

---
+++ u3p11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace u3p11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, temp, sum = 0, rem;
+
+            Console.WriteLine("Enter a number:");
+            n = int.Parse(Console.ReadLine());
+
+            temp = n;
+
+            while (n > 0)
+            {
+                rem = n % 10;
+                sum = sum + (rem * rem * rem);
+                n = n / 10;
+            }
+
+            if (temp == sum)
+            {
+                Console.WriteLine("It is Armstrong");
+            }
+            else
+            {
+                Console.WriteLine("It is not Armstrong");
+            }
+        }
+    }
+}
